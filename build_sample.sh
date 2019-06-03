0476726be0cd2f8eacdb3ea636ab98f2ef13d3d5
Stephen Sherratt <stephen@sherra.tt>
2019-06-03 22:23:28 +0100

    Fix bash scripts

--- build_sample.sh
+++ build_sample.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 cd "$( dirname "${BASH_SOURCE[0]}" )"
-cargo build --manifest-path=nes-samples/Cargo.toml
-cargo run --manifest-path=nes-samples/Cargo.toml --example=$1
+cargo build --manifest-path=nes_samples/Cargo.toml
+cargo run --manifest-path=nes_samples/Cargo.toml --example=$1
