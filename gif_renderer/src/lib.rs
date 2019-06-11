0549a622edc05e3f277a267b4ddaa6adc5ce3ae5
Stephen Sherratt <stephen@sherra.tt>
2019-06-11 22:21:45 +0100

    More accurate timing in gifs

--- gif_renderer/src/lib.rs
+++ gif_renderer/src/lib.rs
@@ -29,7 +29,7 @@ impl<W: io::Write> Renderer<W> {
     }
     pub fn add(&mut self, frame: Frame) {
         let mut gif_frame = gif::Frame::default();
-        gif_frame.delay = 3;
+        gif_frame.delay = 2;
         gif_frame.width = nes_specs::SCREEN_WIDTH_PX;
         gif_frame.height = nes_specs::SCREEN_HEIGHT_PX;
         gif_frame.buffer = Cow::Borrowed(frame.indices());
