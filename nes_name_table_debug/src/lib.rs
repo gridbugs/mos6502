ad479bb5c24f4c0e9ebb9f27e75818b101aa852d
Stephen Sherratt <stephen@sherra.tt>
2019-06-19 21:09:18 +0100

    Fix out of bounds access in debugging library

--- nes_name_table_debug/src/lib.rs
+++ nes_name_table_debug/src/lib.rs
@@ -31,16 +31,18 @@ impl NameTableFrame {
     pub fn indices(&self) -> [u8; NAME_TABLE_TOTAL_PX as usize] {
         let mut indices = self.indices;
         for Scroll { x, y } in self.scroll_by_scanline.iter() {
-            for i in 0..nes_specs::SCREEN_WIDTH_PX {
-                let mut pixel_x = x + i;
-                if pixel_x >= NAME_TABLE_WIDTH_PX {
-                    pixel_x -= NAME_TABLE_WIDTH_PX;
-                }
-                let pixel_y = *y;
-                let pixel_index =
-                    pixel_y as usize * NAME_TABLE_WIDTH_PX as usize + pixel_x as usize;
-                if indices[pixel_index] < nes_palette::NUM_COLOURS as u8 {
-                    indices[pixel_index] += nes_palette::NUM_COLOURS as u8;
+            let pixel_y = *y;
+            if pixel_y < NAME_TABLE_HEIGHT_PX {
+                for i in 0..nes_specs::SCREEN_WIDTH_PX {
+                    let mut pixel_x = x + i;
+                    if pixel_x >= NAME_TABLE_WIDTH_PX {
+                        pixel_x -= NAME_TABLE_WIDTH_PX;
+                    }
+                    let pixel_index =
+                        pixel_y as usize * NAME_TABLE_WIDTH_PX as usize + pixel_x as usize;
+                    if indices[pixel_index] < nes_palette::NUM_COLOURS as u8 {
+                        indices[pixel_index] += nes_palette::NUM_COLOURS as u8;
+                    }
                 }
             }
         }
